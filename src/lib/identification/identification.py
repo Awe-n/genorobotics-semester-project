589999140ce760536d10ae7ff08bd88272accad9
Emilien Ordonneau <45515408+Hbjugo@users.noreply.github.com>
2023-11-29 16:09:54 +0100

    Draft for dl automatically the dbs if not yet installed

--- src/lib/identification/identification.py
+++ src/lib/identification/identification.py
@@ -1,4 +1,4 @@
 from .identification_pipelines.identification_pipeline_blastn import identification_pipeline_blastn
 
-def run_identification():
-    identification_pipeline_blastn()
+def run_identification(input_name: str, db: str = None):
+    identification_pipeline_blastn(input_name, db)
